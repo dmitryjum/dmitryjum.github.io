f6666f7fee34cdc362fd3deb806a710446372eda
Dmitry Jum <xfiler@ymail.com>
2017-07-13 23:33:45 -0400

    fixes https in quote of the day script

--- js/day_quote.js
+++ js/day_quote.js
@@ -1,5 +1,5 @@
 $(function() {
-  $.getJSON('http://quotes.rest/qod.json', function(data) {
+  $.getJSON('https://quotes.rest/qod.json', function(data) {
     var quote = data.contents.quotes[0].quote;
     var author = data.contents.quotes[0].author;
     $('#quote-of-the-day').text('"' + quote + '" Author: ' + author);
