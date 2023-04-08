91f3d56792c638e8fefa54826c4c37f4bcd8cbc6
Dmitry Jum <xfiler@ymail.com>
2023-04-07 20:58:11 -0700

    some updates

--- js/day_quote.js
+++ js/day_quote.js
@@ -1,7 +1,7 @@
-$(function() {
-  $.getJSON('https://quotes.rest/qod.json', function(data) {
-    var quote = data.contents.quotes[0].quote;
-    var author = data.contents.quotes[0].author;
-    $('#quote-of-the-day').text('"' + quote + '" Author: ' + author);
-  });
-})
+// $(function() {
+//   $.getJSON('https://quotes.rest/qod.json', function(data) {
+//     var quote = data.contents.quotes[0].quote;
+//     var author = data.contents.quotes[0].author;
+//     $('#quote-of-the-day').text('"' + quote + '" Author: ' + author);
+//   });
+// })
